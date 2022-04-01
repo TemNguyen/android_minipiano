9760795ec8500338cfd34168de7f6e22ed99d4c0
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-04-01 08:31:01 +0700

    first_commit

---
+++ app/src/main/java/com/example/minipiano/Key.java
@@ -0,0 +1,14 @@
+package com.example.minipiano;
+
+import android.graphics.RectF;
+
+public class Key {
+    public int sound;
+    public RectF rect;
+    public boolean isDown;
+
+    public Key(int sound, RectF rect) {
+        this.sound = sound;
+        this.rect = rect;
+    }
+}
